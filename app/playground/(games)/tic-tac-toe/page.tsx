0e50474f9edad37ec4c9a5a4cab59bdaca040c9d
Heeyeon <heeyeon.lee0@gmail.com>
2025-06-11 01:16:32 -0700

    updated githuburl for plus

--- app/playground/(games)/tic-tac-toe/page.tsx
+++ app/playground/(games)/tic-tac-toe/page.tsx
@@ -7,11 +7,22 @@ import PageContainer from '@/app/components/PageContainer';
 import Toggle from '@/app/components/Toggle';
 
 export default function TicTacToe() {
-  const [isPlusOn, setIsPlusOn] = useState(false);
+  const [isPlusOn, setIsPlusOn] = useState(0);
+
+  const gameMode = [
+    {
+      name: "Tic Tac Toe",
+      githubLink: "https://github.com/heeyeonl/the-website/blob/main/app/components/games/TicTacToeGame.tsx"
+    },
+    {
+      name: "Tic Tac Toe +",
+      githubLink: "https://github.com/heeyeonl/the-website/blob/main/app/components/games/TicTacToePlusGame.tsx"
+    }
+  ]
   return (
-    <PageContainer title={isPlusOn ? "Tic Tac Toe +" : "Tic Tac Toe"} githubLink="https://github.com/heeyeonl/the-website/blob/main/app/components/games/TicTacToeGame.tsx">
+    <PageContainer title={gameMode[isPlusOn].name} githubLink={gameMode[isPlusOn].githubLink}>
       <div className="w-full flex justify-end">
-        <Toggle props={{ isOn: isPlusOn, setIsOn: setIsPlusOn, label: 'Plus' }} />
+        <Toggle props={{ isOn: isPlusOn === 1, setIsOn: () => setIsPlusOn(isPlusOn === 1 ? 0 : 1), label: 'Plus' }} />
       </div>
       {isPlusOn ? <TicTacToePlusGame /> : <TicTacToeGame />}
     </PageContainer>
